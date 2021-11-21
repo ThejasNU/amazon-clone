82e76491f1a821713e6bfb900d4c0060cf9848e0
ThejasNU <thejasnu@gmail.com>
2021-11-21 21:56:37 +0530

    added cart screen and redux to it

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,14 +1,125 @@
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { addToCart } from "../actions/cartActions";
+import MessageBox from "../components/MessageBox";
+
 const CartScreen = (props) => {
 	const productId = props.match.params.id;
 	const qty = props.location.search
 		? Number(props.location.search.split("=")[1])
 		: 1;
+
+	const cart = useSelector((state) => state.cart);
+	const { cartItems } = cart;
+	const dispatch = useDispatch();
+	useEffect(() => {
+		if (productId) {
+			dispatch(addToCart(productId, qty));
+		}
+	}, [dispatch, productId, qty]);
+
+	const removeFromCartHandler = (id) => {};
+
+	const checkoutHandler = () => {
+		props.history.push("/signin?redirect=shipping");
+	};
+
 	return (
-		<div>
-			<h1>Cart Screen</h1>
-			<p>
-				ADD TO CART: ProductID:{productId} Qty:{qty}
-			</p>
+		<div className="row top">
+			<div className="col-2">
+				<h1>Shopping Cart</h1>
+				{cartItems.length === 0 ? (
+					<MessageBox>
+						Cart is empty <Link to="/">Go Shopping</Link>
+					</MessageBox>
+				) : (
+					<ul>
+						{cartItems.map((item) => (
+							<li key={item.product}>
+								<div className="row">
+									<div>
+										<img
+											src={item.image}
+											alt={item.name}
+											className="small"
+										/>
+									</div>
+									<div className="min-30">
+										<Link to={`/product/${item.product}`}>
+											{item.name}
+										</Link>
+									</div>
+									<div>
+										<select
+											value={item.qty}
+											onChange={(e) =>
+												dispatch(
+													addToCart(
+														item.product,
+														Number(e.target.value)
+													)
+												)
+											}
+										>
+											{[
+												...Array(
+													item.countInStock
+												).keys(),
+											].map((x) => (
+												<option key={x + 1}>
+													{x + 1}
+												</option>
+											))}
+										</select>
+									</div>
+									<div>${item.price}</div>
+									<div>
+										<button
+											type="button"
+											onClick={() =>
+												removeFromCartHandler(
+													item.product
+												)
+											}
+										>
+											Delete
+										</button>
+									</div>
+								</div>
+							</li>
+						))}
+					</ul>
+				)}
+			</div>
+			<div className="col-1">
+				<div className="card card-body">
+					<ul>
+						<li>
+							<h2>
+								Subtotal (
+								{cartItems.reduce((a, c) => a + c.qty, 0)}{" "}
+								items) : $
+								{cartItems.reduce(
+									(a, c) => a + c.price * c.qty,
+									0
+								)}
+							</h2>
+						</li>
+						<li>
+							<button
+								type="button"
+								onClick={checkoutHandler}
+								className="primary block"
+								disabled={cartItems.length === 0}
+							>
+								Proceed to Checkout
+							</button>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,10 +4,20 @@ import {
 } from "./reducers/productReducers";
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
-const initialState = {};
+import { cartReducer } from "./reducers/cartReducers";
+
+const initialState = {
+	cart: {
+		cartItems: localStorage.getItem("cartItems")
+			? JSON.parse(localStorage.getItem("cartItems"))
+			: [],
+	},
+};
+
 const reducer = combineReducers({
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
+	cart: cartReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
